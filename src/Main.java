6dfd271106362405188bd2c53b361e497b26b7da
Spolpinka <a421243266@gmail.com>
2023-03-01 09:00:37 +0300

    function + consumer

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.text.DecimalFormat;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Main {
@@ -6,11 +8,29 @@ public class Main {
         System.out.println("Hello world!");
 
         Human ivan = new Human("Ivan");
+//Predicate
+        Predicate<String> isLenghy = x -> x.length() > 5;
+        Predicate<Integer> isPositive = x -> x > 0;
 
-        Predicate<String> isLengh = x -> x.length() > 5;
+        System.out.println(isLenghy.test("ровно"));
+        System.out.println(isLenghy.test("больше"));
+        System.out.println("проверяем число 10101");
+        System.out.println(isPositive.test(10101));
+        System.out.println("проверяем число -100");
+        System.out.println(isPositive.test(-100));
+//Consumer
+        Consumer<String> cons = System.out::println;
+        Consumer<String> consL = s -> System.out.println(s);
+        System.out.println("пробуем вывести имя Ивана");
+        cons.accept(ivan.getName());
+        //Function
+        DecimalFormat decimalFormat = new DecimalFormat("#");
+        Function<Double, Long> func = d -> Long.valueOf(decimalFormat.format(d));
+        //пробуем вывести дробь 1324123.99999
+        System.out.println("пробуем вывести дробь 1324123.99999");
+        System.out.println(func.apply(1324123.99999));
 
-        System.out.println(isLengh.test("ровно"));
-        System.out.println(isLengh.test("больше"));
+
 
     }
 }
